e5b67427b8a39527da84256f6b13a9634442fb4e
TypeCombinator <typecombinator@foxmail.com>
2025-04-12 21:40:40 +0800

    Upgrade Catch2 to v3.8.1

--- cmake/module/Catch2.cmake
+++ cmake/module/Catch2.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare(
   Catch2
-  URL https://github.moeyy.xyz/https://github.com/catchorg/Catch2/archive/refs/tags/v3.7.1.tar.gz
-  URL_HASH MD5=9fcbec1dc95edcb31c6a0d6c5320e098
+  URL https://github.com/catchorg/Catch2/archive/refs/tags/v3.8.1.tar.gz
+  URL_HASH MD5=26689984e0a30ff78b94488c1f849f1f
   DOWNLOAD_EXTRACT_TIMESTAMP ON
 )
 FetchContent_MakeAvailable(Catch2)
