8adcd13da144f043e565fdfa1e54fde9b01e1b97
TypeCombinator <typecombinator@foxmail.com>
2025-05-28 19:14:03 +0800

    Add some bit operations

---
+++ kon/bit.hpp
@@ -0,0 +1,61 @@
+#ifndef BIT_4E5B20B2_B9E4_4DE7_B12A_4D0D6BCA298E
+#define BIT_4E5B20B2_B9E4_4DE7_B12A_4D0D6BCA298E
+
+namespace kon {
+
+template <typename T>
+constexpr T byteswap(T x) noexcept {
+    if constexpr (sizeof(x) == 2) {
+        return __builtin_bswap16(x);
+    } else if constexpr (sizeof(x) == 4) {
+        return __builtin_bswap32(x);
+    } else if constexpr (sizeof(x) == 8) {
+        return __builtin_bswap64(x);
+    } else {
+        static_assert(sizeof(x) <= 8);
+        return 0;
+    }
+}
+
+// Sometimes we need UB!
+template <typename T, bool disable_ub = true>
+constexpr int count_zero(T x) noexcept {
+    if constexpr (disable_ub) {
+        if (x == 0) { // __builtin_clz(0) is UB!
+            return sizeof(x) * 8;
+        }
+    }
+    if constexpr (sizeof(x) <= sizeof(unsigned)) {
+        return __builtin_clz(x);
+    } else if constexpr (sizeof(x) <= sizeof(unsigned long)) {
+        return __builtin_clzl(x);
+    } else if constexpr (sizeof(x) <= sizeof(unsigned long long)) {
+        return __builtin_clzll(x);
+    } else {
+        static_assert(sizeof(x) <= sizeof(unsigned long long));
+        return -1;
+    }
+}
+
+// Sometimes we need UB!
+template <typename T, bool disable_ub = true>
+constexpr int countr_zero(T x) noexcept {
+    if constexpr (disable_ub) {
+        if (x == 0) { // __builtin_ctz(0) is UB!
+            return sizeof(x) * 8;
+        }
+    }
+    if constexpr (sizeof(x) <= sizeof(unsigned)) {
+        return __builtin_ctz(x);
+    } else if constexpr (sizeof(x) <= sizeof(unsigned long)) {
+        return __builtin_ctzl(x);
+    } else if constexpr (sizeof(x) <= sizeof(unsigned long long)) {
+        return __builtin_ctzll(x);
+    } else {
+        static_assert(sizeof(x) <= sizeof(unsigned long long));
+        return -1;
+    }
+}
+
+} // namespace kon
+#endif // bit.hpp
---
+++ tests/unit/bit.cpp
@@ -0,0 +1,76 @@
+#include <kon/bit.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("byteswap", "[bit]") {
+    SECTION("uint16_t") {
+        uint16_t value = 0x1234;
+        REQUIRE(kon::byteswap(value) == 0x3412);
+    }
+    SECTION("uint32_t") {
+        uint32_t value = 0x12345678;
+        REQUIRE(kon::byteswap(value) == 0x78563412);
+    }
+    SECTION("uint64_t") {
+        uint32_t value = 0x12345678;
+        REQUIRE(kon::byteswap(value) == 0x78563412);
+    }
+}
+
+TEST_CASE("count_zero", "[bit]") {
+    SECTION("uint32_t") {
+        uint32_t value = 1;
+        REQUIRE(kon::count_zero(value) == 31);
+        value = 0;
+        REQUIRE(kon::count_zero(value) == 32);
+        value = ~value;
+        REQUIRE(kon::count_zero(value) == 0);
+        value = 0x7FFFFFFF;
+        REQUIRE(kon::count_zero(value) == 1);
+        for (uint32_t i = 0; i < 32; i++) {
+            REQUIRE(kon::count_zero(0x80000000u >> i) == i);
+        }
+    }
+    SECTION("uint64_t") {
+        uint64_t value = 1;
+        REQUIRE(kon::count_zero(value) == 63);
+        value = 0;
+        REQUIRE(kon::count_zero(value) == 64);
+        value = ~value;
+        REQUIRE(kon::count_zero(value) == 0);
+        value = 1ull << 63;
+        for (uint32_t i = 0; i < 64; i++) {
+            REQUIRE(kon::count_zero(value >> i) == i);
+        }
+    }
+}
+
+TEST_CASE("countr_zero", "[bit]") {
+    SECTION("uint32_t") {
+        uint32_t value = 1;
+        REQUIRE(kon::countr_zero(value) == 0);
+        value = 0;
+        REQUIRE(kon::countr_zero(value) == 32);
+        value = ~value;
+        REQUIRE(kon::countr_zero(value) == 0);
+        value = 0xE;
+        REQUIRE(kon::countr_zero(value) == 1);
+        value = 1;
+        for (uint32_t i = 0; i < 32; i++) {
+            REQUIRE(kon::countr_zero(value << i) == i);
+        }
+    }
+    SECTION("uint64_t") {
+        uint64_t value = 1;
+        REQUIRE(kon::countr_zero(value) == 0);
+        value = 0;
+        REQUIRE(kon::countr_zero(value) == 64);
+        value = ~value;
+        REQUIRE(kon::countr_zero(value) == 0);
+        value = 0xE;
+        REQUIRE(kon::countr_zero(value) == 1);
+        value = 1;
+        for (uint32_t i = 0; i < 64; i++) {
+            REQUIRE(kon::countr_zero(value << i) == i);
+        }
+    }
+}
