f2a5cedc1d6e258ab1551d8353b0cf9022a2c6a0
TypeCombinator <typecombinator@foxmail.com>
2025-06-03 11:44:14 +0800

    Rename the inserter to out, and rename mem_cview to mem_view

--- kon/log.hpp
+++ kon/log.hpp
@@ -5,40 +5,40 @@
 namespace kon {
 extern const char base16_encode_lut2[512];
 
-struct mem_cview {
+struct mem_view {
     const void* va;
     std::size_t size;
 };
 } // namespace kon
 
 template <>
-struct fmt::formatter<kon::mem_cview> {
+struct fmt::formatter<kon::mem_view> {
     constexpr auto parse(fmt::format_parse_context& ctx) {
         return ctx.begin();
     }
 
     template <typename FormatContext>
-    auto format(const kon::mem_cview& mem, FormatContext& ctx) const {
+    auto format(const kon::mem_view& mem, FormatContext& ctx) const {
         auto* data = static_cast<const unsigned char*>(mem.va);
         const auto* data_end = data + mem.size;
-        auto inserter = ctx.out();
+        auto out = ctx.out();
         std::size_t line_count = 0;
         for (; data < data_end; data++) {
             if (line_count < 32u) {
                 if ((line_count != 0) && (line_count & 3) == 0) {
-                    *inserter++ = ' ';
+                    *out++ = ' ';
                 }
             } else {
-                *inserter++ = '\n';
+                *out++ = '\n';
                 line_count = 0;
             }
             const char* encode_pair = &kon::base16_encode_lut2[*data << 1];
-            *inserter++ = encode_pair[0];
-            *inserter++ = encode_pair[1];
+            *out++ = encode_pair[0];
+            *out++ = encode_pair[1];
 
             line_count++;
         }
-        return inserter;
+        return out;
     }
 };
 
