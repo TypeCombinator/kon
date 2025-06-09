642350f30be79533fd41ba120ea4ba640a0ed0e1
TypeCombinator <typecombinator@foxmail.com>
2025-06-09 09:46:39 +0800

    Fix unit tests for the bio

--- tests/unit/bio.cpp
+++ tests/unit/bio.cpp
@@ -10,15 +10,15 @@ struct foo {
 } // namespace kon_bio_test
 
 TEST_CASE("base", "[bio]") {
-    uint8_t data[sizeof(kon_bio_test::foo)] = {0x78, 0x56, 0x34, 0x12, 0x34, 0x12, 0x78, 0x56};
+    uint8_t idata[sizeof(kon_bio_test::foo)] = {0x78, 0x56, 0x34, 0x12, 0x34, 0x12, 0x78, 0x56};
     SECTION("read") {
-        kon::bio<kon_bio_test::foo> view{data};
+        kon::bio<kon_bio_test::foo> view{idata};
         REQUIRE(KON_BIO_READ(view, x) == 0x12345678);
         REQUIRE(KON_BIO_READ(view, y) == 0x1234);
         REQUIRE(KON_BIO_READ(view, z) == 0x5678);
     }
     SECTION("read_to") {
-        kon::bio<kon_bio_test::foo> view{data};
+        kon::bio<kon_bio_test::foo> view{idata};
         uint32_t t_x;
         uint16_t t_y;
         uint16_t t_z;
@@ -32,13 +32,13 @@ TEST_CASE("base", "[bio]") {
     }
 
     SECTION("write") {
-        std::uint8_t buffer[sizeof(kon_bio_test::foo)];
-        kon::bio<kon_bio_test::foo> view{data};
+        std::uint8_t odata[sizeof(kon_bio_test::foo)];
+        kon::bio<kon_bio_test::foo> view{odata};
 
         KON_BIO_WRITE(view, 0x12345678, x);
         KON_BIO_WRITE(view, 0x1234, y);
         KON_BIO_WRITE(view, 0x5678, z);
 
-        REQUIRE(std::memcmp(data, buffer, sizeof(data)));
+        REQUIRE(std::memcmp(idata, odata, sizeof(idata)) == 0);
     }
 }
