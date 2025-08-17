bae3d5a3e6611337bcf839bca0710cb2baf29147
TypeCombinator <typecombinator@foxmail.com>
2025-08-17 13:30:40 -0400

    Move the bit iterator into the bit.hpp file

--- tests/unit/bit.cpp
+++ tests/unit/bit.cpp
@@ -1,5 +1,6 @@
 #include <kon/bit.hpp>
-#include <catch2/catch_test_macros.hpp>
+#include <random>
+#include <catch2/catch_template_test_macros.hpp>
 
 TEST_CASE("byteswap", "[bit]") {
     SECTION("uint16_t") {
@@ -106,6 +107,176 @@ TEST_CASE("count_one", "[bit]") {
             REQUIRE(kon::popcount(value) == i);
             REQUIRE(kon::popcount(~value) == 64 - i);
             value |= (static_cast<uint64_t>(1) << i);
+        }
+    }
+}
+
+TEMPLATE_TEST_CASE(
+    "bit_for_each",
+    "[bitset]",
+    std::uint32_t,
+    std::uint64_t,
+    std::int32_t,
+    std::int64_t) {
+    constexpr std::uint32_t bit_number = sizeof(TestType) * 8;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<TestType> dist(std::numeric_limits<TestType>::min());
+
+    uint8_t expect_out[bit_number], out[bit_number];
+    uint8_t expect_count, count;
+
+    for (std::uint32_t times = 10; times > 0; times--) {
+        TestType data = dist(gen);
+
+        auto t_data = data;
+        expect_count = 0;
+        for (std::uint32_t i = 0; i < bit_number; i++) {
+            if (t_data & 1) {
+                expect_out[expect_count] = i;
+                expect_count++;
+            }
+            t_data >>= 1;
+        }
+
+        count = 0;
+        kon::bit_for_each(data, [&out, &count](uint32_t i) {
+            out[count] = i;
+            count++;
+        });
+
+        REQUIRE(count == expect_count);
+        for (std::uint32_t i = 0; i < count; i++) {
+            REQUIRE(out[i] == expect_out[i]);
+        }
+    }
+}
+
+TEMPLATE_TEST_CASE(
+    "bit_reverse_for_each",
+    "[bitset]",
+    std::uint32_t,
+    std::uint64_t,
+    std::int32_t,
+    std::int64_t) {
+    constexpr std::uint32_t bit_number = sizeof(TestType) * 8;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<TestType> dist(std::numeric_limits<TestType>::min());
+
+    uint8_t expect_out[bit_number], out[bit_number];
+    uint8_t expect_count, count;
+
+    for (std::uint32_t times = 10; times > 0; times--) {
+        TestType data = dist(gen);
+
+        auto t_data = data;
+        expect_count = 0;
+        for (std::uint32_t i = 0; i < bit_number; i++) {
+            if (t_data & (static_cast<TestType>(1) << (bit_number - 1))) {
+                expect_out[expect_count] = i;
+                expect_count++;
+            }
+            t_data <<= 1;
+        }
+
+        count = 0;
+        kon::bit_reverse_for_each(data, [&out, &count](uint32_t i) {
+            out[count] = i;
+            count++;
+        });
+
+        REQUIRE(count == expect_count);
+        for (std::uint32_t i = 0; i < count; i++) {
+            REQUIRE(out[i] == expect_out[i]);
+        }
+    }
+}
+
+TEMPLATE_TEST_CASE(
+    "bit_iterator",
+    "[bitset]",
+    std::uint32_t,
+    std::uint64_t,
+    std::int32_t,
+    std::int64_t) {
+    constexpr std::uint32_t bit_number = sizeof(TestType) * 8;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<TestType> dist(std::numeric_limits<TestType>::min());
+
+    uint8_t expect_out[bit_number], out[bit_number];
+    uint8_t expect_count, count;
+
+    for (std::uint32_t times = 10; times > 0; times--) {
+        TestType data = dist(gen);
+
+        auto t_data = data;
+        expect_count = 0;
+        for (std::uint32_t i = 0; i < bit_number; i++) {
+            if (t_data & 1) {
+                expect_out[expect_count] = i;
+                expect_count++;
+            }
+            t_data >>= 1;
+        }
+
+        count = 0;
+        std::uint32_t i;
+        for (kon::bit_iterator it{data}; it.next(i);) {
+            out[count] = i;
+            count++;
+        }
+
+        REQUIRE(count == expect_count);
+        for (std::uint32_t i = 0; i < count; i++) {
+            REQUIRE(out[i] == expect_out[i]);
+        }
+    }
+}
+
+TEMPLATE_TEST_CASE(
+    "bit_reverse_iterator",
+    "[bitset]",
+    std::uint32_t,
+    std::uint64_t,
+    std::int32_t,
+    std::int64_t) {
+    constexpr std::uint32_t bit_number = sizeof(TestType) * 8;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<TestType> dist(std::numeric_limits<TestType>::min());
+
+    uint8_t expect_out[bit_number], out[bit_number];
+    uint8_t expect_count, count;
+
+    for (std::uint32_t times = 10; times > 0; times--) {
+        TestType data = dist(gen);
+
+        auto t_data = data;
+        expect_count = 0;
+        for (std::uint32_t i = 0; i < bit_number; i++) {
+            if (t_data & (static_cast<TestType>(1) << (bit_number - 1))) {
+                expect_out[expect_count] = i;
+                expect_count++;
+            }
+            t_data <<= 1;
+        }
+
+        count = 0;
+        std::uint32_t i;
+        for (kon::bit_reverse_iterator it{data}; it.next(i);) {
+            out[count] = i;
+            count++;
+        }
+
+        REQUIRE(count == expect_count);
+        for (std::uint32_t i = 0; i < count; i++) {
+            REQUIRE(out[i] == expect_out[i]);
         }
     }
 }
