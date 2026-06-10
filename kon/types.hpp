9a5b8a0fa8013112c4b629b76f6e221e63d7ac57
TypeCombinator <typecombinator@foxmail.com>
2026-06-10 11:24:19 +0800

    Add member_type and always_false_v utility templates

--- kon/types.hpp
+++ kon/types.hpp
@@ -6,6 +6,8 @@
 #define TYPES_F8436028_1D08_400B_90D5_B15ACC7C8432
 
 #include <cstddef>
+#include <type_traits>
+#include <utility>
 
 #define KON_CARRAY_SIZE(_carray_) (sizeof(_carray_) / sizeof(_carray_[0]))
 
@@ -15,6 +17,23 @@ template <typename T, std::size_t N>
 constexpr size_t array_size(const T (&)[N]) noexcept {
     return N;
 }
+
+namespace detail {
+template <auto MP>
+struct member_type { };
+
+template <typename T, typename MT, MT T::* MP>
+struct member_type<MP> {
+    using type = MT;
+};
+} // namespace detail
+
+template <auto MP>
+using member_t = detail::member_type<MP>::type;
+
+// Reference: https://www.open-std.org/JTC1/SC22/WG21/docs/papers/2023/p2593r1.html
+template <typename...>
+constexpr bool always_false_v = false;
 
 } // namespace kon
 
