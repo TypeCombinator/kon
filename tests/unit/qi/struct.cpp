31aef1713ed89ecc08c0f864355d348ac81f6421
TypeCombinator <typecombinator@foxmail.com>
2026-08-23 14:02:17 -0400

    Add member_count

---
+++ tests/unit/qi/struct.cpp
@@ -0,0 +1,19 @@
+#include <kon/qi/struct.hpp>
+
+namespace qi_struct_test {
+struct s_foo0 { };
+
+struct s_foo2 {
+    int a[2];
+};
+
+struct s_foo3 {
+    int a;
+    int b;
+    int c;
+};
+
+static_assert(kon::qi::member_count<s_foo0>() == 0);
+static_assert(kon::qi::member_count<s_foo2>() == 2);
+static_assert(kon::qi::member_count<s_foo3>() == 3);
+} // namespace qi_struct_test
