f80146aa6e42153ebd7e2baaa11ec86aeaa28420
TypeCombinator <typecombinator@foxmail.com>
2026-09-05 10:32:29 -0400

    Simplify the tests for struct annotations

--- tests/unit/qi/addon.cpp
+++ tests/unit/qi/addon.cpp
@@ -1,6 +1,7 @@
 #include <kon/qi/struct.hpp>
 
 namespace qi_addon_test {
+
 struct s_foo {
     int a;
     double b;
@@ -18,24 +19,6 @@ struct s_foo {
 };
 
 static_assert(kon::qi::has_internal_addon<s_foo>);
-
-using s_foo_minfo = kon::qi::s_reflect<s_foo>;
-using s_foo_addon = s_foo_minfo::addon_type;
-
-static constexpr auto s_foo_addon_a = s_foo_addon::get_m(s_foo_minfo::addon_tag<0>());
-static_assert(s_foo_addon_a.size() == 2);
-static_assert(s_foo_addon_a.get<0>() == 0);
-static_assert(s_foo_addon_a.get<1>() == nullptr);
-
-static constexpr auto s_foo_addon_b = s_foo_addon::get_m(s_foo_minfo::addon_tag<1>());
-static_assert(s_foo_addon_b.size() == 1);
-static_assert(s_foo_addon_b.get<0>() == 'X');
-
-static constexpr auto s_foo_addon_c = s_foo_addon::get_m(s_foo_minfo::addon_tag<2>());
-static_assert(s_foo_addon_c.size() == 0);
-
-static constexpr auto s_foo_addon_d = s_foo_addon::get_m(s_foo_minfo::addon_tag<3>());
-static_assert(s_foo_addon_c.size() == 0);
 
 struct s_bar {
     int a;
@@ -62,21 +45,49 @@ namespace qi_addon_test {
 static_assert(!kon::qi::has_external_addon<s_foo>);
 static_assert(kon::qi::has_external_addon<s_bar>);
 
-using s_bar_minfo = kon::qi::s_reflect<s_bar>;
-using s_bar_addon = s_bar_minfo::addon_type;
+template <typename T, char V = 'X'>
+consteval bool test_struct_addon() noexcept {
+    using minfo = kon::qi::s_reflect<T>;
+    using addon = minfo::addon_type;
 
-static constexpr auto s_bar_addon_a = s_bar_addon::get_m(s_bar_minfo::addon_tag<0>());
-static_assert(s_bar_addon_a.size() == 2);
-static_assert(s_bar_addon_a.get<0>() == 0);
-static_assert(s_bar_addon_a.get<1>() == nullptr);
+    constexpr auto m0_addon = addon::get_m(minfo::template addon_tag<0>());
+    static_assert(m0_addon.size() == 2);
+    static_assert(m0_addon.template get<0>() == 0);
+    static_assert(m0_addon.template get<1>() == nullptr);
 
-static constexpr auto s_bar_addon_b = s_bar_addon::get_m(s_bar_minfo::addon_tag<1>());
-static_assert(s_bar_addon_b.size() == 1);
-static_assert(s_bar_addon_b.get<0>() == 'X');
+    constexpr auto m1_addon = addon::get_m(minfo::template addon_tag<1>());
+    static_assert(m1_addon.size() == 1);
+    static_assert(m1_addon.template get<0>() == V);
 
-static constexpr auto s_bar_addon_c = s_bar_addon::get_m(s_bar_minfo::addon_tag<2>());
-static_assert(s_bar_addon_c.size() == 0);
+    constexpr auto m2_addon = addon::get_m(minfo::template addon_tag<2>());
+    static_assert(m2_addon.size() == 0);
 
-static constexpr auto s_bar_addon_d = s_bar_addon::get_m(s_bar_minfo::addon_tag<3>());
-static_assert(s_bar_addon_c.size() == 0);
+    constexpr auto m3_addon = addon::get_m(minfo::template addon_tag<3>());
+    static_assert(m2_addon.size() == 0);
+    return true;
+}
+
+static_assert(test_struct_addon<s_foo>());
+static_assert(test_struct_addon<s_bar>());
+
+template <typename T>
+struct ts_x {
+    int a;
+    double b;
+    const char c;
+    char d;
+
+    template <typename addon_host_type = ts_x>
+    struct addon_register {
+        KON_QI_ADDON_INIT();
+
+        KON_QI_ADDON_M(a, 0, nullptr);
+        KON_QI_ADDON_M(b, std::is_same_v<void, T> ? 'X' : 'Y');
+        KON_QI_ADDON_M(c);
+    };
+};
+
+static_assert(test_struct_addon<ts_x<void>>());
+static_assert(test_struct_addon<ts_x<int>, 'Y'>());
+
 } // namespace qi_addon_test
