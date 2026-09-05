c074b3a3a3f8d5866978b5f6af30197a314f7483
TypeCombinator <typecombinator@foxmail.com>
2026-09-05 10:34:51 -0400

    Simplify the annotation API

--- tests/unit/qi/addon.cpp
+++ tests/unit/qi/addon.cpp
@@ -6,43 +6,40 @@ namespace qi_addon_test {
 struct s_foo {
     int a;
     double b;
-    char c;
+    const char c;
     char d;
 
     KON_QI_ADDON_INSIDE(s_foo) {
-        using type = s_foo;
-        KON_QI_ADDON_M(a, 0, 1);
+        KON_QI_ADDON_INIT();
+
+        KON_QI_ADDON_M(a, 0, nullptr);
         KON_QI_ADDON_M(b, 'X');
         KON_QI_ADDON_M(c);
     };
 };
 
-template <typename ADDON, std::size_t I>
-consteval auto addon_get_m() noexcept {
-    constexpr auto m_addr = kon::qi::s_reflect<typename ADDON::type>::sm_pp_info.template get<I>();
-    if constexpr (requires() { ADDON::get_m(kon::qi::addon_tag<m_addr>{}); }) {
-        return ADDON::get_m(kon::qi::addon_tag<m_addr>{});
-    } else {
-        return kon::qi::value_pack<>();
-    }
-}
-
 static_assert(kon::qi::has_internal_addon<s_foo>);
 
-using s_foo_minfo = kon::qi::s_reflect<s_foo>;
+static constexpr auto s_foo_maddrs = kon::qi::s_reflect<s_foo>::sm_maddrs;
 using s_foo_addon = kon::qi::addon_get<s_foo>;
 
-static constexpr auto s_foo_addon_a = addon_get_m<s_foo_addon, 0>();
+static constexpr auto s_foo_addon_a =
+    s_foo_addon::get_m(kon::qi::addon_tag<s_foo_maddrs.get<0>()>{});
+static_assert(s_foo_addon_a.size() == 2);
 static_assert(s_foo_addon_a.get<0>() == 0);
-static_assert(s_foo_addon_a.get<1>() == 1);
+static_assert(s_foo_addon_a.get<1>() == nullptr);
 
-static constexpr auto s_foo_addon_b = addon_get_m<s_foo_addon, 1>();
+static constexpr auto s_foo_addon_b =
+    s_foo_addon::get_m(kon::qi::addon_tag<s_foo_maddrs.get<1>()>{});
+static_assert(s_foo_addon_b.size() == 1);
 static_assert(s_foo_addon_b.get<0>() == 'X');
 
-static constexpr auto s_foo_addon_c = addon_get_m<s_foo_addon, 2>();
+static constexpr auto s_foo_addon_c =
+    s_foo_addon::get_m(kon::qi::addon_tag<s_foo_maddrs.get<2>()>{});
 static_assert(s_foo_addon_c.size() == 0);
 
-static constexpr auto s_foo_addon_d = addon_get_m<s_foo_addon, 3>();
+static constexpr auto s_foo_addon_d =
+    s_foo_addon::get_m(kon::qi::addon_tag<s_foo_maddrs.get<3>()>{});
 static_assert(s_foo_addon_c.size() == 0);
 
 } // namespace qi_addon_test
